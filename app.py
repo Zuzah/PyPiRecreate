2da20af4a09f072bfbd39c1458c7b30418ef1dd5
Zuzah <murtaza.hasni@gmail.com>
2019-10-28 19:40:23 -0400

    Added cms like setup where page is constructed via url

--- app.py
+++ app.py
@@ -2,15 +2,21 @@ import flask
 
 app = flask.Flask(__name__)
 
+
 def register_blueprints():
     from templates.views import home_views
     from templates.views import package_views
+    from templates.views import cms_views
+
     app.register_blueprint(home_views.blueprint)
     app.register_blueprint(package_views.blueprint)
+    app.register_blueprint(cms_views.blueprint)
+
 
 def main():
     register_blueprints()
-    app.run(debug = True)
+    app.run(debug=True)
+
 
 if __name__ == "__main__":
     main()
