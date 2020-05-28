03cc51ea26676b1996859606458fdee203dafe1c
Zuzah <murtaza.hasni@gmail.com>
2020-05-28 14:26:46 -0400

    Restructured _layout.html as a well as its location. Updated .css with new style. Add logic to the package_service.py

--- services/package_service.py
+++ services/package_service.py
@@ -2,7 +2,7 @@
 #function that gets latest pacakges
 def get_latest_packages():
     return [
-        { 'name': 'flask', 'version' : '1.2.3' },
-        { 'name': 'sqlalchemy', 'version' : '2.2.0' },
-        { 'name': 'passlib', 'version' : '3.0.0'}
+        { 'name': 'flask', 'version' : '1.2.3', 'descer':'flask desc' },
+        { 'name': 'sqlalchemy', 'version' : '2.2.0', 'descer': 'alch desc' },
+        { 'name': 'passlib', 'version' : '3.0.0', 'descer': 'other'}
     ]
